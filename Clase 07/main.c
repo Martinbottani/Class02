25c97d1c8a60f796470ec9ed3b5312c30f2237b3
alumno <alumno@lab6pc0>
2018-09-12 22:21:53 -0300

    Clase 07

---
+++ Clase 07/main.c
@@ -0,0 +1,20 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include "utn.h"
+#include "array.h"
+#define CANTIDAD_EDADES 1
+int main()
+{
+    int i;
+    float edades[CANTIDAD_EDADES];
+    char nombres[15][50];
+    for(i = 0; i < CANTIDAD_EDADES; i++)
+    {
+        utn_getNombre(&nombres[15][50], 3, "Ingresa tu nombre: \n", "Error \n");
+        if(utn_getNumeroDecimal(edades, 3, "Ingrese la edad: \n", "Error \n", 200, 0) == -1)
+        {
+            edades[i] = -1;
+        }
+    }
+    return 0;
+}
